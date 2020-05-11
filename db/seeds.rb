6cf8c077927e156263418ab0989c0e797cad01ca
Kta-2k <53724260+Kta-2k@users.noreply.github.com>
2020-05-11 14:56:58 +0900

    変数名の変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,13 @@ require 'yaml'
 
 food = YAML.load_file("db/food.yml")
 
-food.each do |category, i|
-  i.each do |main, j|
-    j.each do |name|
+food.each do |category, main_ingredients|
+  main_ingredients.each do |main_ingredient, names|
+    names.each do |name|
       Food.create!(
         name: name,
         category: category,
-        main_ingredients: main
+        main_ingredients: main_ingredient
       )
     end
   end
