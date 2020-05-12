6c47827688d75aed6790f75964199214d9729015
Kta-2k <53724260+Kta-2k@users.noreply.github.com>
2020-05-12 20:22:10 +0900

    履歴追加機能とフラッシュメッセージの追加

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,13 +1,12 @@
 class FoodsController < ApplicationController
   before_action :set_food, only: %i[show]
+  before_action :food_id, only: %i[show]
 
   def index
-    p 'food params : ', food_params
     redirect_to food_path(random_food) if food_params[:category].present?
   end
 
   def show
-    @food_id = {id: params[:id]}
   end
 
   private
@@ -22,5 +21,9 @@ class FoodsController < ApplicationController
 
   def food_params
     params.permit(:category)
+  end
+
+  def food_id
+    @food_id = params.permit(:id)
   end
 end
--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,17 +1,27 @@
 class HistoriesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_food_id, only: %i[create]
 
   def create
-    p "確認　　#{@food_id}"
-    @history = History.create(user_id: current_user.id, food_id: food_id[:id])
-    if @history.save
-      redirect_to root_path
+    if History.create(user_id: current_user.id, food_id: @food_id, selected_at: Time.now)
       flash[:success] = "保存しました"
+      redirect_to root_path
     else
+      flash[:danger] = "保存できませんでした"
       redirect_to root_path
     end
   end
 
   def destroy
+  end
+
+  private
+  
+  def set_food_id
+    @food_id = food_id_params[:food_id].to_i
+  end
+
+  def food_id_params
+    params.permit(:food_id)
   end
 end
